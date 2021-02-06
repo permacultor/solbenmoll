eb0e54c872e54cf9af401f4f681106e2c0f326cc
Aral Roca Gomez <aral-rg@hotmail.com>
2021-02-06 13:36:52 +0100

    Update prisma model (#17)

--- pages/producte/[[...productId]].tsx
+++ pages/producte/[[...productId]].tsx
@@ -1,7 +1,37 @@
 import { useRouter } from 'next/router'
+import prisma from '../../lib/prisma'
 
-function Producte() {
+export async function getStaticProps({ params }) {
+  const [type, id] = params.productId
+  let product
+
+  if (type === 'cistella') {
+    product = await prisma.basket.findUnique({ where: { id } })
+  } else {
+    product = await prisma.extra.findUnique({ where: { id } })
+  }
+
+  return { props: { product } }
+}
+
+export async function getStaticPaths() {
+  const baskets = await prisma.basket.findMany({ select: { id: true } })
+  const extras = await prisma.extra.findMany({ select: { id: true } })
+  const products = [
+    ...baskets.map((p) => ['cistella', p.id]),
+    ...extras.map((p) => ['extra', p.id]),
+  ]
+
+  return {
+    paths: products.map((productId) => ({ params: { productId } })),
+    fallback: false,
+  }
+}
+
+function Product({ product }) {
   const { query } = useRouter()
+
+  console.log({ product })
 
   return (
     <div className="content">
@@ -12,4 +42,4 @@ function Producte() {
   )
 }
 
-export default Producte
+export default Product
